c89c9e4c630cd426d104e79799f62e0d76d85de8
MartinPa <99693089+MartinPans@users.noreply.github.com>
2024-10-11 02:09:43 +0200

    Plein de petits changements d'ui

--- Assets/Script/ButtonColorChange.cs
+++ Assets/Script/ButtonColorChange.cs
@@ -10,11 +10,12 @@ public class ButtonColorChange : MonoBehaviour, IPointerEnterHandler, IPointerEx
     private TextMeshProUGUI buttonText;
     // Color hovercolor = new Color(90,117,151,255);
     Color originalColor = new Color(0.8196079f,0.8588235f,0.9058824f,1f);
-    Color hoverColor = new Color(0.3529412f, 0.4588236f, 0.5921569f, 1f);
+    Color hoverColor = new Color(0.776f, 0.655f, 0.463f, 1f);
 
     void Start()
     {
         buttonText = GetComponentInChildren<TextMeshProUGUI>();
+        print(hoverColor);
     }
 
     public void OnPointerEnter(PointerEventData eventData) {
